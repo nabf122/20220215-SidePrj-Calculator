eff1012a24cd83a5dd2f1994ee084c7277888980
SQI <SQI@DESKTOP-J6JCNQH>
2022-02-15 16:44:05 +0900

    2nd Commit

--- src/com/song/Calculator.java
+++ src/com/song/Calculator.java
@@ -4,37 +4,79 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener {
+
+	String btn[] = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", ".", "=", "/" };
+	String num = "";
 	
-	public Calculator() {
+	JLabel label;
+	
+	public Calculator() 
+	{
 		super("계산기");
 		
-		// JPanel
-		JPanel mainPanel = new JPanel();
+		// 결과 GUI
+		label = new JLabel("0");
+		label.setHorizontalAlignment(JLabel.RIGHT);
 		
-		// Label
-		JLabel label = new JLabel("숫자를 입력해주세요");
+		// 버튼 패널
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(4,4,2,2));
 		
-		// button
-		JButton btn1 = new JButton("test button");
 		
-		mainPanel.add(label);
-		mainPanel.add(btn1);
+		// 버튼 생성
+		JButton button[] = new JButton[btn.length];
+		for(int i=0;i<btn.length;i++)
+		{
+			button[i] = new JButton(btn[i]);
+			button[i].addActionListener(this);
+			panel.add(button[i]);
+		}
 		
-		add(mainPanel);
-		// size 지정
-		setSize(450, 800);
+		/*
+		button[0].addActionListener(event -> {
+			num = num + "7";
+			label.setText(num);
+        });
+		
+		button[1].addActionListener(event -> {
+			num = num + "8";
+			label.setText(num);
+        });
+        
+        button[i].addActionListener(event -> {
+			num = num + btn[i];
+			label.setText(num);
+        });
+		*/
+		
+		add(label, BorderLayout.NORTH);
+		add(panel, BorderLayout.SOUTH);
+		
+		setSize(250,200);
 		setVisible(true);
-		
-		// 종료 이벤트
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
-	
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Calculator();
+	}
 
-		Calculator cal = new Calculator();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == btn[0])
+		{
+			num = num + btn[0];
+			label.setText(num);
+		}
 		
 	}
 
