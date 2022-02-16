55aaf645b4f8e62ae467e1867c8ed46fdb2bdae6
SQI <SQI@DESKTOP-J6JCNQH>
2022-02-16 14:11:02 +0900

    3th Commit

--- src/com/song/Calculator.java
+++ src/com/song/Calculator.java
@@ -12,12 +12,22 @@ import java.awt.event.ActionEvent;
 public class Calculator extends JFrame implements ActionListener {
 
 	String btn[] = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", ".", "=", "/" };
-	String num = "";
-	String num2 = "";
-	int op = 0;
+
+	// 사칙연산을 가지고 있는 변수
+	String op = "";
+	
+	// 연산 식을 보여주는 변수
+	String disp = "";
+	
+	String disp_set = "";
+	
+	// 실제 연산에 사용되는 변수들
+	int num1, num2;
+	
+	// 최종 결과 값
 	double result = 0;
 	
-	JLabel label;
+	JLabel label, label2;
 	
 	public Calculator() 
 	{
@@ -26,7 +36,7 @@ public class Calculator extends JFrame implements ActionListener {
 		// 결과 GUI
 		label = new JLabel("0");
 		label.setHorizontalAlignment(JLabel.RIGHT);
-		
+				
 		// 버튼 패널
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(4,4,2,2));
@@ -41,23 +51,6 @@ public class Calculator extends JFrame implements ActionListener {
 			panel.add(button[i]);
 		}
 		
-		/*
-		button[0].addActionListener(event -> {
-			num = num + "7";
-			label.setText(num);
-        });
-		
-		button[1].addActionListener(event -> {
-			num = num + "8";
-			label.setText(num);
-        });
-        
-        button[i].addActionListener(event -> {
-			num = num + btn[i];
-			label.setText(num);
-        });
-		*/
-		
 		add(label, BorderLayout.NORTH);
 		add(panel, BorderLayout.SOUTH);
 		
@@ -66,7 +59,25 @@ public class Calculator extends JFrame implements ActionListener {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
+	
+	public int plus(int a, int b) {
 		
+		int num = 0;
+		
+		num = a + b;
+		
+		return num;
+	}
+		
+	public int sub(int a, int b) {
+		
+		int num = 0;
+		
+		num = a - b;
+		
+		return num;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Calculator();
@@ -75,73 +86,49 @@ public class Calculator extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getActionCommand() == btn[0])
-		{
-			num += btn[0];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[1])
-		{
-			num = num + btn[1];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[2])
-		{
-			num = num + btn[2];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[3])
-		{
-			num += btn[3];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[4])
-		{
-			num = num + btn[4];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[5])
-		{
-			num = num + btn[5];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[6])
-		{
-			num = num + btn[6];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[7])
-		{
-			num = num + btn[7];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[8])
-		{
-			num = num + btn[8];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[9])
-		{
-			num = num + btn[9];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[10])
-		{
-			num = num + btn[10];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[11])
-		{
-			num = num + btn[11];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[12])
-		{
-			num = num + btn[12];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[13])
-		{
-			num = num + btn[13];
-			label.setText(num);
-		}else if(e.getActionCommand() == btn[14])
-		{
-			System.out.println(label.getText());
-			
-			label.setText(num);
-		}else
-		{
-			num = num + btn[15];
-			label.setText(num);
-		}
 		
+		if(e.getActionCommand() == "+") {
+			if(op.equals("+")) {
+				num2 = Integer.parseInt(disp_set);
+				result = plus(num1, num2);
+				num1 = (int) result;
+				op ="";
+			} else {
+				disp += "+";
+				label.setText(disp);
+				op = "+";
+				num1 = Integer.parseInt(disp_set);
+				disp_set = "";
+			}
+		}
+		else if(e.getActionCommand() == "-") {
+			if(op.equals("-")) {
+				num2 = Integer.parseInt(disp_set);
+				result = sub(num1, num2);
+				num1 = (int) result;
+				op ="";
+			} else {
+				disp += "+";
+				label.setText(disp);
+				op = "+";
+				num1 = Integer.parseInt(disp_set);
+				disp_set = "";
+			}
+		}
+		else if(e.getActionCommand() == "*") {
+			
+		}
+		else if(e.getActionCommand() == "/") {
+			
+		}
+		else if(e.getActionCommand() == "=") {
+			
+		}
+		else {
+			if(e.getActionCommand() == btn[0])
+			disp += btn[0];
+			disp_set += btn[0];
+			label.setText(disp);
+		}
 	}
-
 }
